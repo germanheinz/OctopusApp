5225f5be211354e2699181db5af77da21859fbe1
germanheinz <heinz.german@gmail.com>
2020-11-16 14:05:02 +0100

    Implementing Auth modules in Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { PagesComponent } from './pages/pages.component';
-import { UserComponent } from './pages/user/user.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 const routes: Routes = [
-  { path: 'login',    component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  imports: [RouterModule.forRoot(routes), PagesRoutingModule, AuthRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
